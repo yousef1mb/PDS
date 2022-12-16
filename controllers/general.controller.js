9924d1cc8a24e179fc208e9276b63423f31ae06e
AbdulazizYas <abdulazizyass@gmail.com>
2022-12-17 02:39:17 +0300

    finish login and logout

--- controllers/general.controller.js
+++ controllers/general.controller.js
@@ -6,9 +6,22 @@ const getLoginPage = async (req, res) => {
   return res.render("login");
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  if (req.body.email === "admin@pds.com") {
+    if (req.body.password === "admin123123") {
+      return res.redirect("/admin");
+    } else {
+      return res.render("loginError");
+    }
+  } else {
+    const customer = { ssn: 753343245 };
+    return res.redirect("/customers/" + customer.ssn);
+  }
+};
 
-const logout = async (req, res) => {};
+const logout = async (req, res) => {
+  return res.redirect("/");
+};
 
 module.exports = {
   getMainPage,
--- routers/general.router.js
+++ routers/general.router.js
@@ -5,7 +5,7 @@ const general = require("../controllers/general.controller");
 
 router.get("/", general.getMainPage); // done
 router.get("/login", general.getLoginPage); // done
-router.post("/login", general.login);
-router.post("/logout", general.logout);
+router.post("/login", general.login); // done
+router.get("/logout", general.logout); // done
 
 module.exports = router;
