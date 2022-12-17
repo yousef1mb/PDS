f43f02e03f3b3ec0a33f9578947decbd8db452ca
AbdulazizYas <abdulazizyass@gmail.com>
2022-12-17 03:44:14 +0300

    delete db

--- controllers/general.controller.js
+++ controllers/general.controller.js
@@ -1,3 +1,5 @@
+const { User } = require("../models/dbObj");
+
 const getMainPage = async (req, res) => {
   return res.render("main");
 };
@@ -7,16 +9,30 @@ const getLoginPage = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  if (req.body.email === "admin@pds.com") {
-    if (req.body.password === "admin123123") {
-      return res.redirect("/admin");
-    } else {
-      return res.render("loginError");
-    }
-  } else {
-    const customer = { ssn: 753343245 };
-    return res.redirect("/customers/" + customer.ssn);
+  const user = await User.getByEmail(req.body.email);
+  if (!user) {
+    return res.render("loginError");
   }
+
+  if (user.Password !== req.body.password) {
+    return res.render("loginError");
+  }
+
+  const isAdmin = await User.isAdmin(user.U_SSN);
+
+  if (isAdmin) {
+    return res.redirect("/admin");
+  } else {
+    return res.redirect("/customers/" + user.U_SSN);
+  }
+  // if (req.body.email === "admin@pds.com") {
+  //   if (req.body.password === "admin123123") {
+  //   } else {
+  //     return res.render("loginError");
+  //   }
+  // } else {
+  //   const customer = { ssn: 753343245 };
+  // }
 };
 
 const logout = async (req, res) => {
---
+++ test.sql
@@ -0,0 +1,4 @@
+INSERT INTO sysUser(U_SSN, Fname, Mname, Lname, Phone, Email, Password)
+           VALUES ('928304', 'Rayyan',
+           ' ','Radda',55345424,'rayyan@gmail.com',
+           '123123');
